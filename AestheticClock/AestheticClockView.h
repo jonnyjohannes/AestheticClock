e172290b723afb935b65d97570aa2a7e634fc112
Jonny Johannes <jonny@jonnyjohannes.com>
2022-11-27 22:37:52 -0500

    Initial Commit

---
+++ AestheticClock/AestheticClockView.h
@@ -0,0 +1,12 @@
+//
+//  AestheticClockView.h
+//  AestheticClock
+//
+//  Created by Jonny on 11/27/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface AestheticClockView : ScreenSaverView
+
+@end
